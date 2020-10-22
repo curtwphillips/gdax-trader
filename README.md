ff141f374de8abbb1c00960f418d868ee1abe73a
curtwphillips <curtwphillips@gmail.com>
2020-10-22 15:15:59 -0400

    catch errors related to account not found on startup

--- README.md
+++ README.md
@@ -5,19 +5,24 @@ This is the gdax automated trading bot. Thoroughly read, understand, and update 
 # Getting Started
 
 ## Node Version
+
 Install Node.js. Node.js v7.x or above is required. The setup is different for different systems. Check https://nodejs.org/en/ for installation instructions. Example install steps for linux:
 
 ## Clone the repository
 
 ## Install App Dependencies
+
 Assuming the cloned files are at ~/, run the following in a terminal to install dependencies:
+
 ```
-cd ~/gdax-trader
+cd ~/gdax-trading-bot
 npm install
 ```
 
 ## Setup the configuration file
-Copy ~/gdax-trader/config/config.js to ~/gdax-trader/lib/config.js
+
+The config file at ~/gdax-trading-bot/config/config.js is an example config file.
+Copy ~/gdax-trading-bot/config/config.js to ~/gdax-trading-bot/lib/config.js
 
 Fill in API data in the lib/config.js file made in the prior step. Find the exports.gdax object in this file. Follow the instructions in the comments above the object about filling in the properties.
 
@@ -25,31 +30,11 @@ Fill in the exports.currencies object with original holdings such as the amount 
 
 exports.currencies controls what currencies are used. USD is mandatory. The coins can be changed. For instance comment out 'LTC-USD': 'LTC', to stop trading with Litecoin, or add 'ETH-USD': 'ETH' to trade with Ethereum.
 
-## Modify the Gdax Node Module
-These changes must be made in the gdax npm node module located at ~/gdax-trader/node_modules/gdax/lib/clients/websocket.js
-```
-var WebsocketClient = function(productIDs, websocketURI, auth, channel) { // added channel parameter line 13
-```
-and
-```
-self.channel = channel; // added channel setting line 16
-```
-and
-```
-if (self.channel) { // added channel logic line 65
-  subscribeMessage.channels = [{ "name": self.channel, "product_ids": self.productIDs }]
-} else {
-  self.product_ids = self.productIDs;
-}
-```
-and
-```
-var sig = signRequest(self.auth, 'GET', '/users/self/verify'); // changed url for auth line 73
-```
-
 ## Run the program
+
 start index.js
+
 ```
-cd ~/gdax-trader
+cd ~/gdax-trading-bot
 node index.js
 ```
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 /*
  * Main program entry point
  */
-const accounts = require('./lib/accounts');
-const api_listener = require('./lib/api_listener');
-const config = require('./lib/config');
-const log = require('./lib/log');
-const orders = require('./lib/orders');
-const products = require('./lib/products');
-const requests = require('./lib/requests');
-const server = require('./server');
-const trader = require('./lib/trader');
+const accounts = require("./lib/accounts");
+const api_listener = require("./lib/api_listener");
+const config = require("./lib/config");
+const log = require("./lib/log");
+const orders = require("./lib/orders");
+const products = require("./lib/products");
+const requests = require("./lib/requests");
+const server = require("./server");
+const trader = require("./lib/trader");
 
 let listeners = [];
 
-async function init () {
+async function init() {
   try {
     await accounts.init();
     await products.init();
     await orders.init();
     requests.init();
     listeners.push(api_listener.listen(config.server.apiPort));
-    log.info('*** Initialization Complete ***');
+    log.info("*** Initialization Complete ***");
   } catch (err) {
     log.error(err.stack);
     exitProcess();
@@ -29,14 +29,15 @@ async function init () {
 
 init();
 
-async function shutDown () {
-  setTimeout(function () { // ensure exit within a few seconds if await takes a while
+async function shutDown() {
+  setTimeout(function () {
+    // ensure exit within a few seconds if await takes a while
     exitProcess();
   }, 2000);
   config.server.shuttingDown = true;
   // cancel any pending gdax orders while shutting down
   if (config.gdax.cancelOnShutdown) {
-    log.info('canceling open orders on gdax');
+    log.info("canceling open orders on gdax");
     orders.cancelAll();
   }
   // stop the api listeners
@@ -47,10 +48,12 @@ async function shutDown () {
       listener.close(function () {
         numClosed++;
         if (numClosed == listeners.length) {
-          log.info('Listeners closed');
+          log.info("Listeners closed");
         }
       });
-      setImmediate(function(){listener.emit('close')});
+      setImmediate(function () {
+        listener.emit("close");
+      });
     });
   }
   // give processes an extra moment to complete
@@ -59,25 +62,20 @@ async function shutDown () {
   }, 1000);
 }
 
-function exitProcess () {
+function exitProcess() {
   process.exit();
 }
 
-process.on('SIGTERM', function () {
+process.on("SIGTERM", function () {
   shutDown();
 });
 
-process.on('SIGINT', function () {
+process.on("SIGINT", function () {
   shutDown();
 });
 
-// catch unhandled promises - out of use for now because it made error tracking harder
-// process.on('unhandledRejection', function (err) {
-//   log.error('unhandledRejection: ' + JSON.stringify(err));
-// });
-
-process.on('unhandledRejection', (reason, p) => {
-  log.debug('Unhandled Rejection at: Promise', p, 'reason:', reason);
+process.on("unhandledRejection", (reason, p) => {
+  log.debug("Unhandled Rejection at: Promise", p, "reason:", reason);
   log.debug(reason.stack);
   // application specific logging, throwing an error, or other logic here
 });
